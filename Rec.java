a20724a30f5860dbeb3fc3fa39f6965a50824332
TechieUpasna <122786334+TechieUpasna@users.noreply.github.com>
2023-01-20 17:10:58 +0530

    Add files via upload

---
+++ Rec.java
@@ -0,0 +1,16 @@
+class Rec
+{
+    public static void main(String args[])
+    {
+        int l,b,area,per;
+        l=10;
+        b=20;
+        area=l*b;
+int p=l+b;
+per=2*p;
+        System.out.println("The lenght of rectangle is: "+l);
+        System.out.println("The breadth of rectangle is: "+b);
+        System.out.println("Area of the rectangle is: "+area);
+System.out.println("perimeter of the rectangle is: "+per);
+    }
+}      
