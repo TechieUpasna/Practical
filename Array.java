f5c9ff87b9af1fa71e1eb1e70563dcf55dc6f0d2
TechieUpasna <122786334+TechieUpasna@users.noreply.github.com>
2023-03-02 16:37:46 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,13 @@
+package lab;
+
+public class Array {
+
+	public static void main(String[] args) {
+		
+String[] names= {"Abhay","Babita","Chikki","Divya","Elena","Falguni","Gargi","Himanshi","Ishika","Jahanvi"};
+for(int i=0;i<names.length;i++) {
+	System.out.println(names[i]);
+}
+	}
+
+}
