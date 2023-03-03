3213de69b8b6fdddd51ed5600714e84aae3b4f2d
TechieUpasna <122786334+TechieUpasna@users.noreply.github.com>
2023-03-03 17:11:15 +0530

    Add files via upload

---
+++ ArrayIndexBound.java
@@ -0,0 +1,13 @@
+package pattern;
+
+public class ArrayIndexBound {
+
+	public static void main(String[] args) {
+		int a[]= {2,5,4,8,9};
+		for(int i=0; i<a.length;i++) {
+			System.out.println(a[i]);
+		}
+
+	}
+
+}
