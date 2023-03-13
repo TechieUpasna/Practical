8734924d5773c4cb3f395580818b3a04e0cafa2e
TechieUpasna <122786334+TechieUpasna@users.noreply.github.com>
2023-03-13 17:19:46 +0530

    Add files via upload

---
+++ LengthMethod.java
@@ -0,0 +1,14 @@
+package strings;
+
+public class LengthMethod {
+
+	public static void main(String[] args) {
+		String s="Upasna Aggarwal";
+		System.out.println("The string length "+s+": "+s.length());  //one way
+		//second way
+		int l=s.length();
+		System.out.println("The string length "+s+": "+l);
+
+	}
+
+}
