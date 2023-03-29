45ba43c95ec2213743d429e33196a5e80dde45b6
TechieUpasna <122786334+TechieUpasna@users.noreply.github.com>
2023-03-29 16:48:29 +0530

    Add files via upload

---
+++ Synchronization.java
@@ -0,0 +1,47 @@
+package thread;
+
+//with or without synchronization
+
+class MyThreadClass{
+	synchronized void print(int num) {
+		for(int i=0;i<=5;i++) {
+			System.out.println(num*i);
+			try {
+				Thread.sleep(500);
+			}
+			catch(Exception e) {
+				System.out.println(e);
+			}
+		}
+	}
+}
+class Thread1 extends Thread{
+	MyThreadClass mt;
+	
+	Thread1(MyThreadClass mt){
+		this.mt=mt;
+	}
+	public void run() {
+		mt.print(5);
+	}
+}
+class Thread2 extends Thread{
+	MyThreadClass mt;
+	Thread2(MyThreadClass mt){
+		this.mt=mt;
+	}
+	public void run() {
+		mt.print(2);
+	}
+}
+public class Synchronization {
+	public static void main(String[] args) {
+		MyThreadClass obj=new MyThreadClass();
+		Thread1 t1=new Thread1(obj);
+		Thread1 t2=new Thread1(obj);
+		t1.start();
+		t2.start();
+
+	}
+
+}
