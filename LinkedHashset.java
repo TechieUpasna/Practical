19ea6a636ca02d510c0114877c0b8196792bdf98
TechieUpasna <122786334+TechieUpasna@users.noreply.github.com>
2023-04-14 18:05:28 +0530

    Add files via upload

---
+++ LinkedHashset.java
@@ -0,0 +1,14 @@
+package collection;
+import java.util.*;
+public class LinkedHashset {
+
+	public static void main(String[] args) {
+		Set<String> s=new LinkedHashSet<String>();
+		s.add(null);
+		s.add("Upasna");
+		s.add("Aggarwal");
+		System.out.println(s);
+
+	}
+
+}
