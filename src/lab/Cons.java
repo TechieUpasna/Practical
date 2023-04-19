fc71526add5965149a1594b679e9c9fddb6daeaa
TechieUpasna <122786334+TechieUpasna@users.noreply.github.com>
2023-04-19 14:48:14 +0530

    Add files via upload

---
+++ src/lab/Cons.java
@@ -0,0 +1,21 @@
+package lab;
+
+public class Cons {
+	int a;
+Cons(){
+	 a=5;
+System.out.println("No parameter");
+
+}
+Cons(String arr)
+{String p=arr;}
+public static void main(String[] args) {
+Cons ob=new Cons(); //object created
+Cons op=new Cons(" "); 
+System.out.println("A= "+ob.a); //copy constructor 
+
+		
+		
+	}
+
+}
