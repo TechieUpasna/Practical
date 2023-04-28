d5081b40ae1771fa99aec393a6fd8bef50711898
TechieUpasna <122786334+TechieUpasna@users.noreply.github.com>
2023-04-28 23:00:21 +0530

    Add files via upload

---
+++ Movie Management/Main2.java
@@ -0,0 +1,11 @@
+package mypackage;
+
+public class Main2 {
+
+	public static void main(String[] args) {
+		Movie obj =new Movie();
+		obj.setMovie();
+		obj.getMovie("Birds of prey",9.00,12.00,250.50);
+	}
+
+}
