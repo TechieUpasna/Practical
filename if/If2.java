4a19464ef6509b2fc267645430af447609ae4237
TechieUpasna <122786334+TechieUpasna@users.noreply.github.com>
2023-05-02 22:53:24 +0530

    Add files via upload

---
+++ if/If2.java
@@ -0,0 +1,11 @@
+class If2
+{
+public static void main(String agrs[])
+{
+String name="Upasna";
+if (name=="Tia")
+{
+System.out.println("My name is Tia");
+}
+}
+}
---
+++ if/If3.java
@@ -0,0 +1,9 @@
+class If3
+{
+public static void main(String agrs[])
+{
+String city="Ghaziabad, UP";
+if(city.contains("UP"))
+{
+System.out.println("You are living in UP");
+}}}
